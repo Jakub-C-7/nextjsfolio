1005b926b7da5b71cd2da622c56d5bbef5c31129
Jakub-C-7 <jakubchamera7@gmail.com>
2023-05-09 22:46:35 +0100

    Added 3d model to top of page, implemented loader code but needs to be reviewed

--- app/components/canvas/Computers.jsx
+++ app/components/canvas/Computers.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
 
-const Computers = () => {
+import { Suspense, useEffect, useState } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { CanvasLoader } from "../Loader";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Model } from "../Scene";
+
+const ComputerCanvas = () => {
+  // const computer = useLoader(GLTFLoader, "./desktop_pc/scene.gltf");
+  // const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return (
-    <div>Computers</div>
-  )
-}
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      {/* <Suspense fallback={<CanvasLoader />}> */}
+      <Suspense>
+        {/* <Suspense fallback={"Loading!!!"}> */}
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Model />
+      </Suspense>
 
-export default Computers
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputerCanvas;
