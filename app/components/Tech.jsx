0021e8dffbe01b9abd63519afc3de64254c99418
Jakub-C-7 <jakubchamera7@gmail.com>
2023-05-11 22:26:25 +0100

    Added Tech Skills section

--- app/components/Tech.jsx
+++ app/components/Tech.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+"use client";
+import { BallCanvas } from "./canvas";
+import ComponentLayout from "./layout";
+import { technologies } from "../constants";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { textVariant } from "../utils/motion";
+import Image from "next/image";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <ComponentLayout>
+      <motion.div variants={textVariant()} id="tech">
+        <p className={styles.sectionSubText}>Current tools in the workshop</p>
+        <h2 className={styles.sectionHeadText}>Technologies</h2>
+      </motion.div>
 
-export default Tech
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+          <div
+            className="w-28 h-28 key={technology.name}"
+            key={technology.name}
+          >
+            <BallCanvas icon={technology.icon} />
+            {/* <div>
+              {technology.name}
+              <img src={technology.icon}></img>
+            </div> */}
+          </div>
+        ))}
+      </div>
+    </ComponentLayout>
+  );
+};
+
+export default Tech;
--- app/components/canvas/Ball.jsx
+++ app/components/canvas/Ball.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  OrbitControls,
+  Decal,
+  Float,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Ball = () => {
+import Image from "next/image";
+
+const Ball = (props) => {
+  const [decal] = useTexture(["./logo.svg"]);
+  // const [decal] = useTexture([props.imgUrl]);
+
+  // console.log(props);
+  // const [colorMap, displacementMap, normalMap, roughnessMap, aoMap] =
+  //   useTexture([props.imgUrl]);
   return (
-    <div>Ball</div>
-  )
-}
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          flatShading
+          map={decal}
+        />
+      </mesh>
+    </Float>
+  );
+};
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop="demand"
+      // shadows
+      // camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          // maxPolarAngle={Math.PI / 2}
+          // minPolarAngle={Math.PI / 2}
+        />
+        <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
