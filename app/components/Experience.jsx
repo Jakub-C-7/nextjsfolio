591da4ba3191eb1c1d9447066b7f07881e3b1a18
Jakub-C-7 <jakubchamera7@gmail.com>
2023-05-11 20:31:32 +0100

    Added imports

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -1,5 +1,8 @@
 "use client";
-// import { VerticalTimelineElement } from "react-vertical-timeline-component";
+import {
+  VerticalTimelineElement,
+  VerticalTimeline,
+} from "react-vertical-timeline-component";
 import { motion } from "framer-motion";
 import "react-vertical-timeline-component/style.min.css";
 import { styles } from "../styles";
